b8ab9bde96279993efac57030d073f97d8f225c8
Andreas Strauman <andreas@strauman.net>
2015-11-09 20:49:50 +0100

    no message

--- lib/ews/types/calendar_item.rb
+++ lib/ews/types/calendar_item.rb
@@ -15,6 +15,7 @@ module Viewpoint::EWS::Types
       start:        [:start, :text],
       end:          [:end, :text],
       location:     [:location, :text],
+      state:        [:appointment_state, :text],
       all_day?:     [:is_all_day_event, :text],
       legacy_free_busy_status: [:legacy_free_busy_status, :text],
       my_response_type:   [:my_response_type, :text],
