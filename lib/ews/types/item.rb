f5bd8d4c353d1108a7a26a6d8550088e62955c8c
Andreas Strauman <andreas@strauman.net>
2015-11-09 21:46:13 +0100

    no message

--- lib/ews/types/item.rb
+++ lib/ews/types/item.rb
@@ -382,8 +382,10 @@ module Viewpoint::EWS::Types
       return [] if users.nil?
       users.collect do |u|
         u[:attendee][:elems].collect do |a|
-          # build_mailbox_user(a[:mailbox][:elems]) if a[:mailbox]
-          a
+          mbox=build_mailbox_user(a[:mailbox][:elems]) if a[:mailbox]
+          rtype=a[:response_type]
+          rtime=a[:last_response_time]
+          {mailbox: mbox, response_type: rtype, response_time: rtime}
         end
       end.flatten.compact
     end
