126545e4330ab8889ef1d0b1eb4e0a17926794f1
Andreas Strauman <andreas@strauman.net>
2015-11-09 23:28:36 +0100

    no message

--- lib/ews/types/item.rb
+++ lib/ews/types/item.rb
@@ -384,7 +384,7 @@ module Viewpoint::EWS::Types
         u[:attendee][:elems].collect do |a|
           # return a[:response_type] if a[:response_type]
           # return a[:last_response_time] if a[:last_response_time]
-          # build_mailbox_user(a[:mailbox][:elems]) if a[:mailbox]
+          build_mailbox_user(a[:mailbox][:elems]) if a[:mailbox]
         end
       end.flatten.compact
     end
@@ -392,7 +392,7 @@ module Viewpoint::EWS::Types
       return [] if users.nil?
       users.collect do |u|
         u[:attendee][:elems].collect do |a|
-          a
+          a[:response_type]
         end
       end.flatten.compact
     end
